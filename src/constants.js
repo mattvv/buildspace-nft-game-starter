e29b92d9ab8a9f5c24dbe1e2519717fc304b6db5
Matt Van <mattvan@hey.com>
2022-02-09 16:39:56 -0400

    finished

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = "0xb588bff5A2D055B153C3D70A836cB44993f5C053";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
